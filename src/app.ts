2253988c7c6cdb8f9803aa645371c3b0a8762d29
Lucas <lucassoares2007@hotmail.com>
2023-05-04 16:02:33 -0300

    build: base project

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import 'express-async-errors';
+import express from 'express';
+import { errorHandler } from './errors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+  next();
+});
+
+app.use(errorHandler);
+
+export default app;
